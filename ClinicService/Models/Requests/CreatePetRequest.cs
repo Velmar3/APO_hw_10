23f965d4f65856e9fdb860779414062d15470b4f
Pavel <pavel.9794@mail.ru>
2023-10-02 10:36:51 +0300

    Added files

---
+++ ClinicService/Models/Requests/CreatePetRequest.cs
@@ -0,0 +1,13 @@
+namespace ClinicService.Models.Requests
+{
+
+    public class CreatePetRequest
+    {
+
+        public int ClientId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
